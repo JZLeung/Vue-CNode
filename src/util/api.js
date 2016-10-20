a903947ec4878634150b865b193be0609918bda6
leung <leungjz@foxmail.com>
2016-10-20 18:07:14 +0800

    清空。

--- src/util/api.js
+++
@@ -1,7 +0,0 @@
-var BASE_HOST = 'https://cnodejs.org/api/v1/';
-
-module.exports = {
-    TOPICS: BASE_HOST+'topics',
-    TOPIC_DETAIL: BASE_HOST+'topic/',
-    TOPIC_UPDATE: BASE_HOST+'topics/update'
-}
