09432071e30b24737d3c0f5123d3f8965fb6d39a
leung <leungjz@foxmail.com>
2016-10-20 18:07:55 +0800

    使用vue-cli重新构建项目。

---
+++ src/components/test.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <p>
+        {{msg}}
+    </p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        msg: 'From test.vue!'
+    }
+  },
+  computed: {},
+  mounted() {},
+  methods: {},
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+    p{
+        color: red;
+    }
+</style>
